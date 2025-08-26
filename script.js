bdd2009720774c0dd602307dc7047c9f9c18a33f
Minakshi Das <minakshidas10001@gmail.com>
2025-08-26 23:52:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const boxes = document.querySelectorAll('.box');
+const gameInfo = document.querySelector('.game-info');
+const newGamebtn = document.querySelector('.btn');
+
+ let CurrentPlayer;
+ let gameGrid;
+
+ const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+ ];
+
+ //let's create a function to initialize the game
+
+ function initGame() {
+    CurrentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    //To empty in UI
+    boxes.forEach ((box, index) => {
+      box.innerText = "";
+      boxes[index].style.pointerEvents = "all";
+      //one more thing is missing
+    });
+    newGamebtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${CurrentPlayer}`;
+ }
+
+ initGame();
+
+ function swapTurn(){
+   if(CurrentPlayer === "X") {
+      CurrentPlayer = "O";
+   } else {
+      CurrentPlayer = "X"
+   }
+
+   //UI update
+   gameInfo.innerText = `Current Player - ${CurrentPlayer}`;
+ }
+
+ function checkGameOver(){
+   
+ }
+
+ function handleClick(index) {
+   if(gameGrid[index] === "") {
+      boxes[index].innerText = CurrentPlayer;
+      gameGrid[index] = CurrentPlayer;
+      boxes[index].style.pointerEvents = "none";
+      //swap
+      swapTurn();
+      checkGameOver();
+   }
+ }
+
+ boxes.forEach((box, index) => {
+    box.addEventListener('click', () => {
+        handleClick(index);
+    });
+ });
+
+ newGamebtn.addEventListener('click', initGame);
